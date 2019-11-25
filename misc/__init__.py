89ccf9e9101271793c720ef6c5e9dcc6aff648c5
BradyFU <54254631+BradyFU@users.noreply.github.com>
2019-11-25 19:56:28 +0800

    Delete __init__.py

--- misc/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Alfred Xiang Wu
-# @Date:   2019-09-25 15:15:49
-# @Breif: 
-# @Last Modified by:   Alfred Xiang Wu
-# @Last Modified time: 2019-09-25 15:16:01
-
-from .util import *
